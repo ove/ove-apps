ove/ove-apps

commit acc8fbc8cc06529181bf941d0de3f19153e46435

HTML Controller should not initialize with default state if state is set (#115)
* HTML Controller should not initialize with default state if state is set

* lint - add a newline

packages/ove-app-html/src/client/control/html.js
@@ -35,5 +35,24 @@ getCSS = function () {
 
 beginInitialization = function () {
     log.debug('Starting controller initialization');
-    OVE.Utils.initControl(Constants.DEFAULT_STATE_NAME, initControl);
+
+    $(document).on(OVE.Event.LOADED, function () {
+        log.debug('Invoking OVE.Event.Loaded handler');
+
+        window.ove.state.load().then(function () {
+            const currentState = window.ove.state.current;
+
+            if (currentState) {
+                log.debug('Initializing controller with state:', currentState);
+                initControl(currentState);
+            } else {
+                log.debug('Missing state information - loading default state');
+                OVE.Utils.initControl(Constants.DEFAULT_STATE_NAME, initControl);
+            }
+        }).catch(function () {
+            log.debug('State load failed - loading default state');
+            // If the promise is rejected, that means no current state is existing.
+            OVE.Utils.initControl(Constants.DEFAULT_STATE_NAME, initControl);
+        });
+    });
 };

commit 839adf10cd3e72e049eb88306ad8da8bbfb362cd

Fixing regression issue introduced in PR #115 by @jamesscottbrown

packages/ove-app-html/src/client/control/html.js
@@ -47,12 +47,12 @@ beginInitialization = function () {
                 initControl(currentState);
             } else {
                 log.debug('Missing state information - loading default state');
-                OVE.Utils.initControl(Constants.DEFAULT_STATE_NAME, initControl);
+                OVE.Utils.initControlOnDemand(Constants.DEFAULT_STATE_NAME, initControl);
             }
         }).catch(function () {
             log.debug('State load failed - loading default state');
             // If the promise is rejected, that means no current state is existing.
-            OVE.Utils.initControl(Constants.DEFAULT_STATE_NAME, initControl);
+            OVE.Utils.initControlOnDemand(Constants.DEFAULT_STATE_NAME, initControl);
         });
     });
 };