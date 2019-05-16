d3cd06244d38163d2e9c7a9ec6f4fa4d43a0bac9
James Scott-Brown <james@jamesscottbrown.com>
2019-05-16 16:46:00 +0100

    remove unneeded code copied from the HTML app

--- packages/ove-app-qrcode/src/client/constants/qrcode.js
+++ packages/ove-app-qrcode/src/client/constants/qrcode.js
@@ -5,27 +5,10 @@ const Constants = {
     DEFAULT_STATE_NAME: 'OVE',
 
     /**************************************************************
-                                Server
-    **************************************************************/
-    SOCKET_REFRESH_DELAY: 5000, // Unit: milliseconds
-    SOCKET_READY_WAIT_TIME: 3000, // Unit: milliseconds
-    HTTP_HEADER_CONTENT_TYPE: 'Content-Type',
-    HTTP_CONTENT_TYPE_JS: 'application/javascript',
-    HTTP_CONTENT_TYPE_JSON: 'application/json',
-
-    /**************************************************************
                                 Common
     **************************************************************/
-    OPERATION_SYNC_DELAY: 350, // Unit: milliseconds
     CONTENT_DIV: '.wrapper',
     APP_NAME: 'qrcode'
-};
-
-/**************************************************************
-                            Enums
-**************************************************************/
-Constants.Operation = {
-    REFRESH: 'refresh'
 };
 
 exports.Constants = Constants;
