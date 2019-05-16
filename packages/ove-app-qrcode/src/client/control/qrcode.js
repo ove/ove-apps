543e4905e93e40d19743d408f3f606b4b32cd8d8
James Scott-Brown <james@jamesscottbrown.com>
2019-05-16 14:53:51 +0100

    remove comment

--- packages/ove-app-qrcode/src/client/control/qrcode.js
+++ packages/ove-app-qrcode/src/client/control/qrcode.js
@@ -1,5 +1,3 @@
 getCSS = function () {
-    // Unlike most apps, the HTML app's controller renders the HTML page at a fixed
-    // height and width.
     return { width: '100%', height: '100%' };
 };
